86d36ebe6c644019f40744aafe166b43043fe988
Guilherme Reis <guilhermereis9876@gmail.com>
2020-09-16 01:40:45 -0300

    Umbler

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,12 @@ import Axios from "axios";
 
 const api = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Authorization",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Content-Type": "application/json;charset=UTF-8",
+  },
 });
 
 export default api;
