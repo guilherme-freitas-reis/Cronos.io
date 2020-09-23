a05c4e1279db9936ec83945e018214aa942bfdee
Guilherme Reis <guilhermereis9876@gmail.com>
2020-09-23 09:40:19 -0300

    cors

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ const api = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Authorization",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Content-Type": "application/json",
   },
 });
 
